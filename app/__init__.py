1cafa4d9200f12c7bb0b6b14d5f2e41361a0e312
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-06-02 22:50:05 -0400

    Added Flask route to load each page

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/<name>')
+def show_profile(name):
+    return render_template('index.html', title=name, url=os.getenv("URL"))
+

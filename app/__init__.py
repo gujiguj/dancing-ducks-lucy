613b533e009768a045b7b2c83f7b0fc72a4100fa
Mia Yan <68654939+yanbmia@users.noreply.github.com>
2022-06-04 22:03:59 -0400

    added hobbies page
    *info needs to be added though

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+# fellow's pages route
+# input the dictionary of a fellow's information using fellows[name]
+@app.route('/fellow/<name>')
+def show_profile(name):
+    return render_template('profile.html', title=fellows[name]['first']+' '+fellows[name]['last'], fellow=fellows[name], url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def show_hobby():
+    return render_template('hobbies.html', title="Hobbies", fellows=fellows)
+
+@app.route('/map')
+def show_map():
+    return render_template('map.html', title="Travel", fellows=fellows)

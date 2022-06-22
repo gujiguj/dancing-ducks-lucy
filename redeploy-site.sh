9bf5f2510a47f69ab82deebf97806a45b1a320e8
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-06-22 15:39:13 -0400

    trying to resolve merge conflicts

---
+++ redeploy-site.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# kill all tmux sessions
+tmux kill-server
+
+# enter repository
+cd dancing-ducks-lucy
+
+# make sure repository is up to date
+git fetch && git reset origin/main --hard
+
+# install requirements into the virtualenv
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+deactivate
+#echo "deactivate successful? $?"
+
+# start new detached tmux session to run the website
+# use tmux send-keys function to send commands to detached tmux session
+NAME=portfolio
+tmux new -d -s $NAME
+tmux send-keys -t $NAME 'source python3-virtualenv/bin/activate' ENTER
+tmux send-keys -t $NAME 'flask run --host=0.0.0.0' ENTER

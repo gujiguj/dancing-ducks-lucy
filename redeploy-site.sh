effd38d2fc701bd0ebbba08782446d3586aa70a8
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-07-07 00:05:30 -0400

    update redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -9,17 +9,8 @@ cd dancing-ducks-lucy
 # make sure repository is up to date
 git fetch && git reset origin/main --hard
 
-# install requirements into the virtualenv
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-# deactivate
-# echo "deactivate successful? $?"
+# remove existing docker containers
+docker compose -f docker-compose.prod.yml down
 
-# start new detached tmux session to run the website
-# use tmux send-keys function to send commands to detached tmux session
-# NAME=portfolio
-# tmux new -d -s $NAME
-# tmux send-keys -t $NAME 'source python3-virtualenv/bin/activate' ENTER
-# tmux send-keys -t $NAME 'flask run --host=0.0.0.0' ENTER
-systemctl restart myportfolio
+# run docker compose to spin up containers with updated files
+docker compose -f docker-compose.prod.yml up --build
